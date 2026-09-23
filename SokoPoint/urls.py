c02a510c5739563f5165893b5e27e7be8e8aab24
Student <student@example.com>
2026-09-23 14:19:13 +0200

    Fix drf_spectacular URL imports

--- SokoPoint/urls.py
+++ SokoPoint/urls.py
@@ -5,6 +5,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 from . import views
 
 urlpatterns = [
@@ -15,6 +20,11 @@ urlpatterns = [
     path('cart/', include('cart.urls')),
     path('api/', include('api.urls')),
     path('', views.home, name='home'),
+
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('api/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
